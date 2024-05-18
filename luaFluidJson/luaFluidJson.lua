334bf054f339c0fe6c051052d008c672a8648088
Mateus <mateusmoutinho01@gmail.com>
2024-05-18 04:26:23 -0300

    at

---
+++ luaFluidJson/luaFluidJson.lua
@@ -0,0 +1,28 @@
+
+
+
+---@class LuaFluidJson
+---@field load_from_string fun(str:string): any 
+---@field load_from_file fun(str:string):any
+---@field dumps_to_string fun(entry:table): string 
+---@field dumps_to_file fun(entry:table)
+
+
+local info = debug.getinfo(1, "S")
+local path = info.source:match("@(.*/)") or ""
+
+
+local lib_path = ''
+
+if os.getenv("HOME") then
+    lib_path = path.."luaFluidJson.so"
+else
+    perror("undefined os")
+end 
+
+local load_lua = package.loadlib(lib_path, "load_lua")
+
+---@type LuaFluidJson
+local lib = load_lua()
+
+return lib
