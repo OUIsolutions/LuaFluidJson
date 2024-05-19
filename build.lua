5cb21f1d2c92c96dff72c3af174d204d80ce080e
Mateus <mateusmoutinho01@gmail.com>
2024-05-19 02:52:31 -0300

    att

--- build.lua
+++ build.lua
@@ -1,3 +1,4 @@
 
-os.execute("gcc -Wall -shared -fpic -o luaFluidJson/luaFluidJson.so  src/main.c")
+os.execute("gcc -Wall  -shared -fpic -o luaFluidJson/luaFluidJson.so  src/main.c")
 
+os.execute("x86_64-w64-mingw32-gcc -Wall  -shared -fpic -o luaFluidJson/luaFluidJson.dll  src/main.c")
