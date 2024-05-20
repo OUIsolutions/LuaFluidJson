9d0cb4ad7ba261bc1c53191785ad0fddba2c9fce
Mateus <mateusmoutinho01@gmail.com>
2024-05-19 21:33:16 -0300

    aa

--- build.lua
+++ build.lua
@@ -124,7 +124,7 @@ local function main()
     dtw.copy_any_overwriting(side_effect_copy_path,SIDE_EFFECT);
     dtw.remove_any(side_effect_copy_path)
 
-    os.execute("zip -r luaFluidJson.zip src/")
+    os.execute("zip -r luaFluidJson.zip luaFluidJson/")
 end
 
 
