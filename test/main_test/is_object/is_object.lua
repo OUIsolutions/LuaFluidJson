bd2bb4ecc9ad16199fd6849e053793890ff45a7f
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-26 07:10:04 -0300

    att

--- test/main_test/is_object/is_object.lua
+++
@@ -1,9 +0,0 @@
-
-local json = require("luaFluidJson/luaFluidJson")
-
-local test = {1,2,3}
-print(json.is_table_a_object(test))
-
-local test2 = {a=20,b=30}
-
-print(json.is_table_a_object(test2))
--- test/main_test/load_from_string/load_from_string.lua
+++
@@ -1,8 +0,0 @@
-local json = require("luaFluidJson/luaFluidJson")
-
-local test = '{"name":"mateus","age":27}'
-
-local parsed = json.load_from_string(test);
-
-print("name:"..parsed.name)
-print("age:"..parsed.age)
