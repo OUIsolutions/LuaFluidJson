94d3f72245c7fc0c256d73ceacc42b45e80415db
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-26 07:39:21 -0300

    att

--- src/extra/fdefine.extra.c
+++ src/extra/fdefine.extra.c
@@ -8,7 +8,7 @@
 
 
 bool lua_json_fluid_table_is_object(LuaCEmbedTable *table){
-    long size = lua.tables.get_size(table);
+    long size = LuaCEmbedTable_get_full_size(table);
     for(int i =0; i < size;i++){
         if(lua.tables.has_key(table,i) == false){
             return false;
@@ -17,7 +17,7 @@ bool lua_json_fluid_table_is_object(LuaCEmbedTable *table){
     return true;
 }
 LuaCEmbedResponse * lua_json_fluid_is_table_a_object(LuaCEmbed *args){
-    LuaCEmbedTable *table = lua.args.get_table(args,0);
+    LuaCEmbedTable *table = LuaCEmbed_get_arg_table(args,0);
     if(lua.has_errors(args)){
         char*error= lua.get_error_message(args);
         return lua.response.send_error(error);
@@ -27,11 +27,11 @@ LuaCEmbedResponse * lua_json_fluid_is_table_a_object(LuaCEmbed *args){
 }
 
 LuaCEmbedResponse * lua_json_fluid_set_null_code(LuaCEmbed *args){
-    char *new_nil_code = lua.args.get_str(args,0);
-    if(lua.has_errors(args)){
-        char*error= lua.get_error_message(args);
-        return lua.response.send_error(error);
+    char *new_nil_code = LuaCEmbed_get_str_arg(args,0);
+    if(LuaCEmbed_has_errors(args)){
+        char*error= LuaCEmbed_get_error_message(args);
+        return LuaCEmbed_send_error(error);
     }
-    lua.globals.set_string(args, PRIVATE_LUA_FLUID_JSON_NULL_CODE_GLOBAL_VAR, new_nil_code);
+    LuaCEmbed_set_global_string(args, PRIVATE_LUA_FLUID_JSON_NULL_CODE_GLOBAL_VAR, new_nil_code);
     return NULL;
 }
