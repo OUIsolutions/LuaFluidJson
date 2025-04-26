0bdc18d8debacd449c5de8dd4167d6d8cfa8ae47
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-26 07:11:17 -0300

    att

---
+++ build/main.lua
@@ -0,0 +1,34 @@
+
+function main()
+    darwin.dtw.remove_any("release")
+
+    install_dependencies()
+
+
+    darwin.silverchain.generate({
+        src = "src",
+        project_short_cut="LuaFluidJson",
+        tags = { "dependencie", "consts", "macros", "globals", "fdeclare", "fdefine" },
+        implement_main = false
+    })
+
+  local no_dep_amalgamation = darwin.camalgamator.generate_amalgamation_with_callback("src/one.c",
+        function(import, path)
+            if import == "src/imports/../dependencie.dependencies.h"  then
+                return "dont-include"
+            end
+            return "include-once"
+
+        end,
+        MAX_CONNTENT,
+        MAX_RECURSION
+    )
+    darwin.dtw.write_file("release/luaFluidJson_no_dep.c", no_dep_amalgamation)
+
+    darwin.dtw.copy_any_overwriting("extra/starter.lua","release/luaFluidJson/luaFluidJson.lua")
+    os.execute("gcc src/one.c -Wall -shared  -fpic -o release/luaFluidJson/luaFluidJson.so")
+
+    --zip the folder 
+    os.execute("cd release && zip -r luaFluidJson.zip luaFluidJson")
+    darwin.dtw.remove_any("release/luaFluidJson")
+end
