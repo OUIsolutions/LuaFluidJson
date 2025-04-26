afa7ee8236468dc448fbbb15ea475d78f40271e3
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-26 11:59:58 -0300

    att

--- teste.lua
+++ teste.lua
@@ -1,2 +1,18 @@
 
-r = require("release/luaFluidJson/luaFluidJson")
+
+-- Create a new container machine
+local machine = darwin.ship.create_machine("debian:latest")
+-- Configure container runtime
+machine.provider = "sudo docker"
+-- Add build-time commands
+machine.add_comptime_command("apt update")
+machine.add_comptime_command("apt install -y gcc ")
+
+machine.start({
+    flags = {
+        "--network=host","-it"
+    },
+    volumes = {
+        { ".", "/teste" }
+    },
+})
