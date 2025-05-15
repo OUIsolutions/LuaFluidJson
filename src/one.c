92c9a895e18d4d87180cd6061f0db1b7f61d234b
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-05-14 22:49:02 -0300

    att

--- src/one.c
+++ src/one.c
@@ -2,7 +2,6 @@
 #include "imports/imports.fdefine.h"
 
 int load_lua_fluid_json(lua_State *state){
-    printf("loading lua fluid json\n");
     LuaCEmbed * l  = newLuaCEmbedLib(state);
     LuaCEmbed_set_global_string(l, PRIVATE_LUA_FLUID_JSON_NULL_CODE_GLOBAL_VAR, LUA_FLUID_JSON_NULL_CODE_VALUE);
 
