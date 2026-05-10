e79d4ac924c0d70fd23b7271860498617ef35fda
augustofrade <aquaaticnaotrava@gmail.com>
2026-05-10 16:11:45 -0300

    Moved project building to new darwinconf style & Add type annotations build

---
+++ build/main_build.lua
@@ -0,0 +1,36 @@
+function main_build()
+    darwin.dtw.remove_any("release")
+    install_dependencies()
+
+    darwin.silverchain.generate({
+        src = "src",
+        project_short_cut = "LuaFluidJson",
+        tags = { "dependencie", "consts", "macros", "globals", "fdeclare", "fdefine" },
+        implement_main = false
+    })
+
+    local no_dep_amalgamation = darwin.camalgamator.generate_amalgamation_with_callback("src/one.c",
+        function(import, path)
+            if import == "src/imports/../dependencie.dep.h" then
+                return "dont-include"
+            end
+            return "include-once"
+        end,
+        MAX_CONNTENT,
+        MAX_RECURSION
+    )
+
+    darwin.dtw.write_file("release/luaFluidJson_no_dep.c", no_dep_amalgamation)
+
+    darwin.dtw.copy_any_overwriting("extra/starter.lua", "release/luaFluidJson/luaFluidJson.lua")
+    os.execute("gcc src/one.c -ldl -shared -fpic -o release/luaFluidJson/luaFluidJson.so")
+    os.execute("cd release && zip -r luaFluidJson.zip luaFluidJson")
+end
+
+darwin.add_recipe({
+    name = "main",
+    description = "Builds the LuaFluidJson module",
+    outs = { "release/luaFluidJson" },
+    inputs = { "src/" },
+    callback = main_build
+})
